41cc848b2236ef147a836953dd7e3b31198439f8
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-01 21:43:20 -0300

    Declaração de variaveis

---
+++ 02_Variaveis/DeclaracaoVariaveis.cs
@@ -0,0 +1,29 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+	static void Main()
+	{
+		/*
+		Declaração de variável
+		*/
+		int a;
+		//Inicialização da variavel
+		a = 22;
+		Console.WriteLine(a);
+		
+		//Reatribuição de valor na variavel
+		a = 56;
+		Console.WriteLine(a);
+		
+		//Outras formas de declaração de variavel
+		
+		//Decalrando e inicializando a variavel
+		int b = 925;
+		Console.WriteLine(b);
+		
+		int y = 10, x = 20;
+		int z = y + x;
+		Console.WriteLine(z);
+	}
+}
