0ebb45fb320a84f340818367316c2788302584d3
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-04 19:44:53 -0300

    Programa em C# com variavel decimal formatado no formato da moeda

---
+++ 02_Variaveis/DeclaracaoVariaveisDecimalFormatado.cs
@@ -0,0 +1,14 @@
+using System;
+
+class DeclaracaoVariaveisDecimalFormatado
+{
+
+	static void Main(string [] args)
+	{
+		decimal x = 0.999m;
+		decimal y = 9999999999999999999999999999m;
+		Console.WriteLine("Minha quantia = {0:C}", x);
+		Console.WriteLine("Sua quantia = {0:C}", y);
+	}
+
+}
