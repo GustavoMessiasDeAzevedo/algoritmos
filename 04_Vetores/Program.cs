658dd7c21652fa93df95930ad6e228d80009f9e9
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-08 20:39:04 -0300

    Declaração de vetores e loops com for e foreach.

--- 04_Vetores/Program.cs
+++ 04_Vetores/Program.cs
@@ -9,8 +9,29 @@ namespace _04_Vetores
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hellow World!");
+         {
+            string nome = "Fulano de tal!";
+            Console.WriteLine(nome);
+
+            string[] nomes = { "Fulano de tal", "Beltrano", "Sicrano", "João", "José", "Maria" };
+            Console.WriteLine(nomes[0]);
+            Console.WriteLine(nomes[1]);
+            Console.WriteLine(nomes[2]);
+
+            //Loop FOR
+            for (int i = 0; i < nomes.Length; i++) { 
+                Console.WriteLine("{0}° Nome: {1}", i+1, nomes[i]);
+            }
+
+            for(int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine("Número: {0}",i);
+            }
+
+            //loop foreach 
+            foreach (string varNome in nomes) { 
+                Console.WriteLine("Nome: {0}",varNome);
+            }
         }
     }
 }
