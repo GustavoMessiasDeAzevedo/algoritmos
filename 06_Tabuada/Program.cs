3764826698286b62ff15c1ceba8ed2367a1b458a
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-08 21:52:41 -0300

    Tabuada usando for

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int multiplicando;
+
+            Console.WriteLine("Digite um número: ");
+            multiplicando = int.Parse(Console.ReadLine());
+
+            for (int multiplicador = 0; multiplicador <= 10; multiplicador++) {
+
+                Console.WriteLine("{0} x {1} = {2}", multiplicando, multiplicador, multiplicando*multiplicador);
+            }
+        }
+    }
+}
