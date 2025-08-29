613658c0a2549014ab9eee44ff019eea2c50cb83
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-08-28 22:00:32 -0300

    Refatoração

--- 09_Calendario/Program.cs
+++ 09_Calendario/Program.cs
@@ -16,98 +16,11 @@ namespace _09_Calendario
 
             Console.WriteLine("Digite o ano");
             int ano = int.Parse(Console.ReadLine());
+            imprimirCalendario(ano);
 
-
-            ImprimirCalendario(ano);
         }
 
-        public static int[] RetornaFeriados(int mes, int ano)
-        {
-            int[] feriados = new int[15];
-            int indice = 0;
-
-
-            DateTime pascoa = DomingoDePascoa(ano);
-
-            DateTime carnaval = pascoa.AddDays(-47);
-            DateTime sextaFeiraSanta = pascoa.AddDays(-2);
-            DateTime corpusChristi = pascoa.AddDays(60);
-
-            if (carnaval.Month == mes)
-            {
-                feriados[indice++] = carnaval.Day;
-            }
-            if (sextaFeiraSanta.Month == mes)
-            {
-                feriados[indice++] = sextaFeiraSanta.Day;
-            }
-            if (corpusChristi.Month == mes)
-            {
-                feriados[indice++] = corpusChristi.Day;
-            }
-            if (pascoa.Month == mes)
-            {
-                feriados[indice++] = pascoa.Day;
-            }
-
-            Array.Sort(feriados);
-            if (mes == 1)
-            {
-                feriados[indice++] = 1;
-            }
-            else if (mes == 4)
-            {
-                feriados[indice++] = 21;
-                feriados[indice++] = 4;
-            }
-            else if (mes == 5)
-            {
-                feriados[indice++] = 1;
-            }
-            else if (mes == 9)
-            {
-                feriados[indice++] = 7;
-            }
-            else if (mes == 10)
-            {
-                feriados[indice++] = 12;
-            }
-            else if (mes == 11)
-            {
-                feriados[indice++] = 2;
-                feriados[indice++] = 15;
-                feriados[indice++] = 20;
-            }
-            else if (mes == 12)
-            {
-                feriados[indice++] = 25;
-            }
-
-            return feriados;
-        }
-
-        public static DateTime DomingoDePascoa(int ano)
-        {
-            int a = ano % 19;
-            int b = ano / 100;
-            int c = ano % 100;
-            int d = b / 4;
-            int e = b % 4;
-            int f = (b + 8) / 25;
-            int g = (b - f + 1) / 3;
-            int h = (19 * a + b - d - g + 15) % 30;
-            int i = c / 4;
-            int k = c % 4;
-            int l = (32 + 2 * e + 2 * i - h - k) % 7;
-            int m = (a + 11 * h + 22 * l) / 451;
-            int mes = (h + l - 7 * m + 114) / 31;
-            int dia = ((h + l - 7 * m + 114) % 31) + 1;
-
-            return new DateTime(ano, mes, dia);
-        }
-
-     
-        public static void ImprimirCalendario(int ano)
+        private static void imprimirCalendario(int ano)
         {
             for (int mes = 1; mes <= 12; mes++)
             {
@@ -181,5 +94,91 @@ namespace _09_Calendario
             Console.WriteLine("A Sexta-feira Santa ocorre 2 dias antes da Páscoa");
             Console.WriteLine("Corpus Christi ocorre 60 dias após a Páscoa");
         }
+
+        public static int[] RetornaFeriados(int mes, int ano)
+        {
+            int[] feriados = new int[15];
+            int indice = 0;
+
+
+            DateTime pascoa = DomingoDePascoa(ano);
+
+            DateTime carnaval = pascoa.AddDays(-47);
+            DateTime sextaFeiraSanta = pascoa.AddDays(-2);
+            DateTime corpusChristi = pascoa.AddDays(60);
+
+            if(carnaval.Month == mes)
+            {
+                feriados[indice++] = carnaval.Day;  
+            }
+            if(sextaFeiraSanta.Month == mes)
+            {
+                feriados[indice++] = sextaFeiraSanta.Day;
+            }
+            if(corpusChristi.Month == mes)
+            {
+                feriados[indice++] = corpusChristi.Day;  
+            }
+            if(pascoa.Month == mes)
+            {
+                feriados[indice++] = pascoa.Day;  
+            }
+
+            Array.Sort(feriados);
+            if (mes == 1)
+            {
+                feriados[indice++] = 1;
+            }
+            else if (mes == 4)
+            {
+                feriados[indice++] = 21;
+                feriados[indice++] = 4;
+            }
+            else if (mes == 5)
+            {
+                feriados[indice++] = 1;
+            }
+            else if (mes == 9)
+            {
+                feriados[indice++] = 7;
+            }
+            else if (mes == 10)
+            {
+                feriados[indice++] = 12;
+            }
+            else if (mes == 11)
+            {
+                feriados[indice++] = 2;
+                feriados[indice++] = 15;
+                feriados[indice++] = 20;
+            }
+            else if (mes == 12)
+            {
+                feriados[indice++] = 25;
+            }
+
+            return feriados;
+        }
+
+        public static DateTime DomingoDePascoa(int ano)
+        {
+            int a = ano % 19;
+            int b = ano / 100;
+            int c = ano % 100;
+            int d = b / 4;
+            int e = b % 4;
+            int f = (b + 8) / 25;
+            int g = (b - f + 1) / 3;
+            int h = (19 * a + b - d - g + 15) % 30;
+            int i = c / 4;
+            int k = c % 4;
+            int l = (32 + 2 * e + 2 * i - h - k) % 7;
+            int m = (a + 11 * h + 22 * l) / 451;
+            int mes = (h + l - 7 * m + 114) / 31;
+            int dia = ((h + l - 7 * m + 114) % 31) + 1;
+
+            return new DateTime(ano, mes, dia);
+        }
+
     }
 }
